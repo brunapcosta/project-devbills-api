29c1ef263e1c8c11c92dab5ad1b48dedd5fabf7c
brunapcosta <brunaa.jornalismo@gmail.com>
2024-10-08 17:46:07 -0300

    config-eslint-prettier-end

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,4 @@ const app = express();
 
 app.use(json());
 
-app.listen(3333, () => console.log('🚀 App is running at port 3333!'))
+app.listen(3333, () => console.log('🚀 App is running at port 3333!'));
